c54ad7837ab1b4b1bf3ae83f1022ac8e91f8cc0e
Victor Querette <vquerette@gmail.com>
2017-12-01 10:33:40 +0100

    shlag

--- pychess.py
+++ pychess.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 
 pygame.init()
 window = pygame.display.set_mode((1024, 720))
+bg = pygame.image.load("imgres/board.jpg").convert()
+window.blit(bg, (0,0))
+pygame.display.flip()
 
 while(1):
     continue
