c4c8f13cd6ab7180546b8fac5b4be227b0efd2f9
Victor Querette <vquerette@gmail.com>
2017-12-05 11:55:07 +0100

    TAHAN

--- board.py
+++ board.py
@@ -1,14 +1,40 @@
 from pieces import *
 
+class Square:
+
+    def __init__(self, xpos, ypos):
+        self.occupyingPiece = None
+        self.xpos = xpos
+        self.ypos = ypos
+
+    def __str__(self):
+        return {
+            0 : 'a',
+            1 : 'b',
+            2 : 'c',
+            3 : 'd',
+            4 : 'e',
+            5 : 'f',
+            6 : 'g',
+            7 : 'h'
+        }[self.xpos] + str(self.ypos+1)
+
 class Player:
+
     def __init__(self, color):
         self.color = color
 
 class PieceSet:
+
     def __init__(self, player):
         if player.color == "black":
-            pawnRow = 7
+            pawnRow = 6
         else:
-            pawnRow = 2
+            pawnRow = 1
         for i in range(8):
             self.pawns.append(pawn(i+1, pawnRow))
+
+class Board:
+
+    def __init__(self):
+        
--- pieces.py
+++ pieces.py
@@ -1,28 +1,42 @@
+from board import Square
+
 class Piece:
+
     def __init__(self, xpos, ypos):
         self.xpos = xpos
         self.ypos = ypos
+        square = Square(xpos, ypos)
+
+    def moveTo(newSquare):
+        self.square.occupyingPiece = None
+        self.square = newSquare
 
 class Pawn(Piece):
+
     def __init__(self, xpos, ypos):
         Piece.__init__(xpos, ypos)
 
 class Rook(Piece):
+
     def __init__(self, xpos, ypos):
         Piece.__init__(xpos, ypos)
 
 class Knight(Piece):
+
     def __init__(self, xpos, ypos):
         Piece.__init__(xpos, ypos)
 
 class Bishop(Piece):
+
     def __init__(self, xpos, ypos):
         Piece.__init__(xpos, ypos)
 
 class Queen(Piece):
+
     def __init__(self, xpos, ypos):
         Piece.__init__(xpos, ypos)
 
 class King(Piece):
+
     def __init__(self, xpos, ypos):
         Piece.__init__(xpos, ypos)
