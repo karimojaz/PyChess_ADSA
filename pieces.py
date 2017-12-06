198a2eecf30818966f433f5f14de12471009ef7e
Victor Querette <vquerette@gmail.com>
2017-12-06 22:03:38 +0100

    suce

--- pieces.py
+++ pieces.py
@@ -31,18 +31,22 @@ class Knight(Piece):
 
     def __init__(self, xpos, ypos, owner):
         Piece.__init__(self, xpos, ypos, owner)
+        self.sprite = pygame.image.load("imgres/" + ("bn.png" if (self.owner.color == "black") else "wn.png")).convert_alpha()
 
 class Bishop(Piece):
 
     def __init__(self, xpos, ypos, owner):
         Piece.__init__(self, xpos, ypos, owner)
+        self.sprite = pygame.image.load("imgres/" + ("bb.png" if (self.owner.color == "black") else "wb.png")).convert_alpha()
 
 class Queen(Piece):
 
     def __init__(self, xpos, ypos, owner):
         Piece.__init__(self, xpos, ypos, owner)
+        self.sprite = pygame.image.load("imgres/" + ("bq.png" if (self.owner.color == "black") else "wq.png")).convert_alpha()
 
 class King(Piece):
 
     def __init__(self, xpos, ypos, owner):
         Piece.__init__(self, xpos, ypos, owner)
+        self.sprite = pygame.image.load("imgres/" + ("bk.png" if (self.owner.color == "black") else "wk.png")).convert_alpha()
